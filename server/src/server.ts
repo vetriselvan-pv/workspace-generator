a4b68420eed8526c9130b42cbfeffae38d0c7b41
Vetriselvan Panneerselvam <85449760+vetriselvan-pv@users.noreply.github.com>
2026-02-22 21:55:32 +0530

    env and package

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,21 @@
+import dotenv from "dotenv";
 import { app } from "./app.js";
 import { env } from "./config/env.js";
+import cors from "cors";
 
-app.listen(env.port, () => {
-  console.log(`Server running on http://localhost:${env.port}`);
-});
+dotenv.config();
 
+app.use(
+  cors({
+    origin: process.env.URL,
+  }),
+);
+
+const PORT = process.env.PORT || 3000;
+if (process.env.NODE_ENV !== "production") {
+  app.listen(env.port, () => {
+    console.log(`Server running on http://localhost:${env.port}`);
+  });
+}
+
+export default app;
